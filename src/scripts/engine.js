6ab1862a65aaee0fcf53455ca65eeeceb7804712
Olavo15 <olavoregis169@gmail.com>
2024-10-09 15:58:50 -0300

    End

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -257,6 +257,9 @@ function init() {
 
     drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer);
+
+    const bgm =document.getElementById("bgm");
+    bgm.play();
 }
 
 init();
